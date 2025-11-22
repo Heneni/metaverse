32e6c6188d01a13d80ad90769fefdc7a25f605e5
Heneni <mark@heneniart.com>
2025-11-21 19:58:56 -0500

    Update vite.config.ts with new Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./",
-  base: "/",
+  base: "./", // switched to relative for Vercel + local asset paths
   build: {
     outDir: "dist"
   },
